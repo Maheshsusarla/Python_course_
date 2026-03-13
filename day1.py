02c726f38ea653037c1c5bc451f2809a5a4091f3
Mahesh Susarla <maheshsusarla5@gmail.com>
2026-03-13 21:12:34 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,62 @@
+#day1:
+#     induction
+#day2:
+#     python download 
+#     python and anaconda navigator
+print("Hello World")
+print(10+5)
+
+#variables: store the values
+a=6
+b="mahesh"
+print(a+b)
+print("a")
+print(a)
+
+#comments
+#this code is for adding 2 values-single line comment
+a=10
+b=20
+print(a+b)
+#multiline comment
+""" 
+this is a 
+multiline 
+comment
+""" 
+#data types:int,float,string,bool
+a=10
+b="mahi"
+c=10.0
+d=True
+e=[2,34,4]#it is not a data type it is a data structure
+
+#find the data type
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+#input function
+name=input("Enter Name")
+print(name)
+name=int(input("Enter Name"))
+print(name)
+
+#operators
+a=10
+b=30
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a//b)
+print(a**b)
+
+sep="@" #seperate the values this @symbol
+print("Mahesh","mahi",sep="@")
+
+end=""
+print("Hello",end=" ")
+print("Mahesh","mahi")
