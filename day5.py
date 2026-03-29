be859efac83fea7cc83147dead69bded5f1d3dc0
Mahesh Susarla <maheshsusarla5@gmail.com>
2026-03-29 12:14:13 +0530

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,99 @@
+#sting basics
+a="String"
+# print(a[0])
+# print(a[1:6])
+
+# print(len(a))
+
+# print(a[::-1])
+
+
+# a="mam"
+# b=a[::-1]
+# if a==b:
+#     print("Palindrome")
+# else:
+#     print("Not")
+
+a="Mahi"
+b="mahesh"
+# print(a+" "+b)
+# print(a*3)
+# print(a+4)-error
+
+text=("Python is a fun")
+# print("fun" in text)
+# print("fun"  not in text)
+# print("java"  not in text)
+
+# print(text.upper())
+# print(text.lower())
+
+# print(text.title()) -first word is captital 
+
+# print(text.strip()) -remove unwanted spaces
+
+# print(text.replace("P","j"))
+# print(text.replace("Python","java"))
+
+# print(text.split())
+
+# text1="education"
+# for ch in "aeiou":
+#     text1=text1.replace(ch,"*")
+# print(text1)
+
+# name=input("Enter the name :")
+# clean=name.strip().title()
+# print(clean)
+
+#list
+lis=["apple","mango",True]
+lis1=[1,23,4,5,6,7,8]
+# print(lis[0])
+# print(lis[1:3])
+
+#append
+# lis.append("Mahesh")
+# lis.append("Mahesh")
+# print(lis)
+#remove
+# lis.remove("apple")
+# print(lis)
+#insert
+# lis.insert(0,"apple")
+# print(lis)
+#extend
+# lis.extend(["mahi",54])
+# # lis.extend([lis1])
+# print(lis)
+
+#pop
+# lis.pop(0)
+# print(lis)
+
+#clear
+# lis.clear()
+# print(lis)
+
+# for i in range(len(lis1)):
+#     print(i,lis1[i])
+
+# for i in lis1:
+#     print(i)
+
+#sort
+# lis1.sort()
+# print(lis1)
+#reversr
+# lis1.reverse()
+# print(lis1)
+# #min,max,sum
+# print(min(lis1))
+# print(max(lis1))
+# print(sum(lis1))
+
+#nested list
+li=[[1,2],[3,4]]
+print(li[0])
+print(li[0][0])
---
+++ day5task.py
@@ -0,0 +1,55 @@
+#take a sentence and find how many vowels in have 
+# sentences=input("Enter a name :")
+# vowels="aeiouAEIOU"
+# total=0
+# for ch in sentences:
+#     if ch in vowels:
+#         total+=1
+# print(total)
+
+#2 reverse a sting without a slicing
+# text=input("Enter the name :")
+# res=" "
+# for ch in text:
+#     res=ch+res
+# print(res)
+
+#3 take a sentence replace every with underscore
+# sentence=input("Enter the name :")
+# result=sentence.replace(" ","_")
+# print(result)
+
+#4 take a comma and seperated string convert into list
+# data=input("Enter tha dada :")
+# result=data.split(",")
+# print(result)
+
+
+# List problems
+#1 create a list a five fav movies and print the second one 
+# movie=["pusha1","pushpa2","pushpa3","july","salar"]
+# # print(movie[1])
+# #2 add one movie using append and remove one movie using remove function
+# movie.append("salar2")
+# print(movie)
+# movie.remove("july")
+# print(movie)
+
+#3 sort a list of numbers in aesc and desc order
+# lis=[1,4,3,5,6,3,6,4,5,78,3,]
+# lis.sort()
+# print(lis)
+# lis.reverse()
+# print(lis)
+
+# 4 take five integer in in user and add list and print  they sum
+# a=int(input("Enter the number : "))
+# b=int(input("Enter the number : "))
+# c=int(input("Enter the number : "))
+# d=int(input("Enter the number : "))
+# e=int(input("Enter the number : "))
+# num=[a,b,c,d,e]
+# print(sum(num))
+#5 acces element from nested list
+# lis=[[1,2,3,34,5],[5,6,7,8,9]]
+# print(lis[0][0])
