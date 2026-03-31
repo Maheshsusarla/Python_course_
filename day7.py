cc8bc8b5b32fc0e0fb1413f228fd4275dd7bc9dc
Mahesh Susarla <maheshsusarla5@gmail.com>
2026-03-31 18:44:30 +0530

    Add files via upload

---
+++ day7.py
@@ -0,0 +1,54 @@
+# function : is a block of code
+# A function is a block of code which only runs when it is called.
+
+# A function can return data as a result.
+
+# A function helps avoiding code repetition.
+# def name(): #define function
+#     print("Hello")
+# name() #calling function
+
+# def play():
+#     print("Play the game")
+# play()
+
+
+# def add():
+#     print(5+1)
+# add()
+
+# def add(a,b): #parameter
+#     print(a+b)
+# add(45,23) #argument 
+# c=add(3,4)
+# print(c)
+
+# def add(a,b):
+#     return a+b
+# c=add(2,4)
+# print(c)
+
+
+# def add(a,b):
+#     return a+b
+# def sub(a,b):
+#     return a-b
+# def mul(a,b):
+#     return a*b
+# def div(a,b):
+#     return a/b
+# c=add(2,3)
+# print(c)
+
+def name(hi="mahesh"):
+    print(hi)
+name()
+name("Mahi")
+
+def f(name,age):
+    print(name,age)
+f(age=51,name="mahi")
+
+
+#global variable and local variable
+
---
+++ day7task.py
@@ -0,0 +1,11 @@
+#write function to find a fact
+a=int(input("Enter the number :"))
+fact=1
+if a>=0:
+    for i in range(1,a+1):
+        fact*=i
+        print(a,fact)
+
+#2 function to count vowels instr
+#3 function return max of number
+#4 function to chech if number prime or not
